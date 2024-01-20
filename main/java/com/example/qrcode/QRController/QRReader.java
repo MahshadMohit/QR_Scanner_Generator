2c200ed5de62fbdbaa6da72e80f700495baa60c7
Asoo <126341039+Maashooly@users.noreply.github.com>
2024-01-20 19:47:21 +0330

    QRReader

---
+++ main/java/com/example/qrcode/QRController/QRReader.java
@@ -0,0 +1,38 @@
+package com.example.qrcode.QRController;
+
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import javax.imageio.ImageIO;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.BinaryBitmap;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.MultiFormatReader;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.NotFoundException;
+import com.google.zxing.Result;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.common.HybridBinarizer;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+
+public class QRReader {
+
+    public static String readQR(String path) throws FileNotFoundException, IOException,
+            NotFoundException {
+        BinaryBitmap binaryBitmap = new BinaryBitmap(new HybridBinarizer(new BufferedImageLuminanceSource(ImageIO.read(new FileInputStream(path)))));
+
+        Result result = new MultiFormatReader().decode(binaryBitmap);
+
+        return result.getText();
+    }
+
+
+}
